359cdaadee0624e3f93039de6d4bd70a8b8ab45e
Gaurav Daga <ankitgauravdaga@gmail.com>
2026-06-07 00:10:18 +0530

    John: 123-456-7890 Jane: 987-654-3210 Doe: 555-555-5555 Not found 123-456-7890

--- pythonScripts/phonebook.py
+++ pythonScripts/phonebook.py
@@ -1,8 +1,5 @@
 phNum = {"John": "123-456-7890", "Jane": "987-654-3210", "Doe": "555-555-5555"}
-print(["John"])
-
 for name, number in phNum.items():
     print(f"{name}: {number}")
-
-
 print(phNum.get("Abe", "Not found"))
+print(phNum.get("John", "Not found"))
