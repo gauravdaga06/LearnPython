e22fa55da8a1f8fe5bb3655b49f7a4dfc6d3c201
Gaurav Daga <ankitgauravdaga@gmail.com>
2026-06-06 22:40:43 +0530

    loops

--- pythonScripts/condition.py
+++ pythonScripts/condition.py
@@ -5,3 +5,16 @@ elif age >= 20:
     print("You are an adult.");
 else:
     print("You are a child.");
+
+names = ["Alice", "Bob", "Charlie", "David", "Eve"];
+
+for name in names:
+    print(name);
+
+for i , name in enumerate(names):
+    print(f"{i}: {name}");
+
+
+
+for i in range(5):
+    print(i);
---
+++ pythonScripts/loops.py
@@ -0,0 +1,4 @@
+lang = ["java", "python", "c++", "javascript"];
+
+for i, language in enumerate(lang):
+        print(f"{i+1}: {language}");
