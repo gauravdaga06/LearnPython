5ccf3feefd7c0773b24ecdee63d7506e79ec02df
Gaurav Daga <ankitgauravdaga@gmail.com>
2026-06-07 00:46:55 +0530

    output 5.7 3.2 4.5 6.0 Total cost of fruits: 19.4 Expensive items (cost > 5): ['apple', 'grape']

---
+++ pythonScripts/inventory.py
@@ -0,0 +1,19 @@
+fruits = {"apple" : 5.7, "banana" : 3.2, "orange" : 4.5, "grape" : 6.0}
+
+def total_cost(prices):
+    total = 0
+    for price in (fruits.values()):
+        print(price)
+        total = total + price
+    return total
+
+print(f"Total cost of fruits: {total_cost(fruits)}")
+
+def expensive_items(prices , threshold):
+    expensive = []
+    for item, price in prices.items():
+        if price > threshold:
+            expensive.append(item)
+    return expensive
+
+print(f"Expensive items (cost > 5): {expensive_items(fruits, 5)}")
