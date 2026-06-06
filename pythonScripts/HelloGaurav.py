94e2484bf2aee65d22af6d25a5bd9eab34c4aae1
Gaurav Daga <ankitgauravdaga@gmail.com>
2026-06-06 22:30:11 +0530

    condition in python

--- pythonScripts/HelloGaurav.py
+++ pythonScripts/HelloGaurav.py
@@ -5,3 +5,8 @@ print(f"My name is {name}");
 age = 25;
 print(age);
 print(f"My name is {name} and I am {age} years old.");
+
+height = 5.9;
+print(height);
+print(f"My name is {name}, I am {age} years old and my height is {height} feet.");
+print(type(name));print(type(age));print(type(height));
---
+++ pythonScripts/condition.py
@@ -0,0 +1,7 @@
+age= 13;
+if age >= 13 and age <= 19:
+    print("You are a teenager.");
+elif age >= 20:
+    print("You are an adult.");
+else:
+    print("You are a child.");
