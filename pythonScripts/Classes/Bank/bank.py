271d4b627e3aff89b833d16adc791d378a53f3ce
Gaurav Daga <ankitgauravdaga@gmail.com>
2026-06-12 07:55:10 +0530

    done

--- pythonScripts/Classes/Bank/bank.py
+++ pythonScripts/Classes/Bank/bank.py
@@ -1,14 +1,14 @@
-class InsufficientFundsException(Exception):
+class InsufficientFundsError(Exception):
     pass
 
-def withdraw(balance : float , amount : float ):
-    if (amount>balance):
-        raise InsufficientFundsException(f"Current Balance : {balance} is less than requested amount : {amount}")
+def withdraw(balance : float , amount : float ) -> float:
+    if amount>balance:
+        raise InsufficientFundsError(f"Current Balance : {balance} is less than requested amount : {amount}")
     else:
         return balance-amount
     
 
 try:
     x = withdraw(100,200)
-except InsufficientFundsException as error:
+except InsufficientFundsError as error:
     print(error)
